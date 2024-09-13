db74d42e91f46a8ef94ebb0d2b9f8d3841c505b0
Avery <averynewkirk@gmail.com>
2024-09-13 15:54:24 -0400

    Added index page and base css file

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+
+@app.route('/home')
+def home():
+    return '<h1> HOME PAGE <h1>'
