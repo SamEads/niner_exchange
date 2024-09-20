bebf64b5c9f470b742300a681f08eda4e013529f
Avery <averynewkirk@gmail.com>
2024-09-20 15:24:39 -0400

    added database schema

---
+++ data/script.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Users (
+    id SERIAL PRIMARY KEY,
+    niner_username VARCHAR(255) UNIQUE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+)
+
+CREATE TABLE Listing (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
+
+)
