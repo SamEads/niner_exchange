e418bea7fa810e520bfc0655713086f9d3554b07
tatataToua <toualee10@gmail.com>
2024-09-20 16:32:10 -0400

    login/ register
    Somewhat complete login and registration page

--- app.py
+++ app.py
@@ -13,4 +13,8 @@ def home():
 
 @app.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template('login.html')
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
