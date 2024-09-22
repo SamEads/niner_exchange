7fba4a4b1428a4a13840d846e83013286020115e
Avery <averynewkirk@gmail.com>
2024-09-21 23:29:31 -0400

    Hooked up db with app and added some auth with bcrypt.

---
+++ data/models.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+
+#data model for the user 
+class Users(db.Model):
+
+
+    def __init__(self,username,email,password_hash):
+        self.username = username
+        self.email = email
+        self.password_hash = password_hash
+        self.first_name = 'placeholder'
+
+    
+    id = db.Column(db.Integer, primary_key=True)  
+    username = db.Column(db.String(255), unique=True, nullable=False) 
+    first_name = db.Column(db.String(50), nullable=False)  
+    email = db.Column(db.String(255), unique=True, nullable=False) 
+    password_hash = db.Column(db.String(255), nullable=False)
