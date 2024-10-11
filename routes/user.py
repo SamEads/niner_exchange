c5335f760bacb265e54222169470e34faaa34d77
Avery <averynewkirk@gmail.com>
2024-10-11 16:35:23 -0400

    added rating system

--- routes/user.py
+++ routes/user.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, session, redirect
-from data.models import Users
+from data.models import Users,Ratings
 from utils.helpers import level
 
 user_bp = Blueprint('user', __name__)
@@ -23,6 +23,7 @@ def user(username):
     acc_name = user.username
     class_lv = level(int(user.class_level))
     member_since = str(user.created_at)[:4]
+    rating = Ratings.get_rating(user.id)
 
     # Render user page
-    return render_template('user.html', acc_name=acc_name, class_lv=class_lv, member_since=member_since)
+    return render_template('user.html', acc_name=acc_name, class_lv=class_lv, member_since=member_since,rating=rating)
