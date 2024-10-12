e0dfb946d573b4dfa1b506a01b6748b94cc3b5bf
MannyLee23 <mannyelee@gmail.com>
2024-10-12 13:39:12 -0400

    updated user.py file

--- routes/user.py
+++ routes/user.py
@@ -1,29 +1,36 @@
-from flask import Blueprint, render_template, session, redirect
-from data.models import Users,Ratings
+from flask import Blueprint, render_template, session, redirect, request
+from data.models import Users, Ratings
 from utils.helpers import level
 
 user_bp = Blueprint('user', __name__)
 
-# Route to render home page
+#route to render home
 @user_bp.route('/')
 def home():
     return render_template('home.html')
 
-# Route to render user page
+#route to listings
+@user_bp.route('/listings', methods=['GET'])
+def listings():
+    query = request.args.get('query')  
+    
+    return render_template('listings.html')  
+
+
 @user_bp.route('/user/<username>')
 def user(username):
-    # Query user from database
+ 
     user = Users.query.filter_by(username=username).first()
 
-    # If user does not exist, render home page
+   
     if user is None:
         return redirect('/')
 
-    # Extract user details
+
     acc_name = user.username
     class_lv = level(int(user.class_level))
     member_since = str(user.created_at)[:4]
     rating = Ratings.get_rating(user.id)
 
-    # Render user page
-    return render_template('user.html', acc_name=acc_name, class_lv=class_lv, member_since=member_since,rating=rating)
+  
+    return render_template('user.html', acc_name=acc_name, class_lv=class_lv, member_since=member_since, rating=rating)
