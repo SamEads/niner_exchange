a701b5b7c183842be977777a78ad43b77f90e4c0
tatataToua <toualee10@gmail.com>
2024-10-31 17:35:06 -0400

    fixed merge conflicts in init.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,6 +3,7 @@ from .user import user_bp
 from .listing import listing_bp
 from .messaging import messaging_bp
 from .inbox import inbox_bp
+from .setting import setting_bp
 
 def register_blueprints(app):
     app.register_blueprint(auth_bp)
@@ -10,3 +11,4 @@ def register_blueprints(app):
     app.register_blueprint(listing_bp)
     app.register_blueprint(messaging_bp)
     app.register_blueprint(inbox_bp)
+    app.register_blueprint(setting_bp)
