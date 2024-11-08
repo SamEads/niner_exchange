ecf27fc4b6de7626774488c3b2547ffd2ac7ce27
SamEads <eads.samuel@gmail.com>
2024-11-08 15:32:11 -0500

    No sending messages to yourself, fool.

--- routes/messaging.py
+++ routes/messaging.py
@@ -4,14 +4,26 @@ from data.models import Users, Messages, db
 # Create blueprint for messaging related routes
 messaging_bp = Blueprint('messaging', __name__)
 
-# Route to render the messaging page
-@messaging_bp.route('/messages/<recipient>', methods=['GET'])
+@messaging_bp.get('/messages/<recipient>')
 def view_messages(recipient):
+    # Session username
     sender = session.get('username')
+    
+    # If sender is not logged in, redirect to login page.
     if not sender:
         flash('You need to be logged in to view messages.')
         return redirect(url_for('auth.login'))
+        
+    # If sender is the same as recipient, redirect to home.
+    if sender == recipient:
+        return redirect('/')
 
+    # If recipient does not exist, redirect to home.
+    recipient_user = Users.query.filter_by(username=recipient).first()
+    if not recipient_user:
+        return redirect('/')
+
+    # Get messages between sender and recipient
     messages = Messages.query.filter(
         (Messages.sender == sender) & (Messages.recipient == recipient) |
         (Messages.sender == recipient) & (Messages.recipient == sender)
@@ -20,16 +32,26 @@ def view_messages(recipient):
     return render_template('messaging.html', messages=messages, recipient=recipient)
 
 # Route to handle sending a message
-@messaging_bp.route('/send-message', methods=['POST'])
+@messaging_bp.post('/send-message')
 def send_message():
+    # Session username
     sender = session.get('username')
+    
+    # Recipient username
     recipient_username = request.form.get('recipient')
+    
+    # Message content
     message_content = request.form.get('message')
 
     # Check if sender is logged in
     if not sender:
         flash('You need to be logged in to send a message.')
         return redirect(url_for('auth.login'))
+
+    # Prevent sending message to self
+    if sender == recipient_username:
+        flash('You cannot send messages to yourself.')
+        return redirect(url_for('messaging.view_messages', recipient=recipient_username))
 
     # Check if recipient exists
     recipient = Users.query.filter_by(username=recipient_username).first()
@@ -45,33 +67,6 @@ def send_message():
     # Create and save the message
     new_message = Messages(sender=sender, recipient=recipient.username, content=message_content)
     db.session.add(new_message)
-    db.session.commit() 
-
-    return redirect(url_for('messaging.view_messages', recipient=recipient_username))
-
-'''
-# Route to handle editing a message
-@messaging_bp.route('/edit-message/<int:message_id>', methods=['POST'])
-def edit_message(message_id):
-    sender = session.get('username')
-    if not sender:
-        flash('You need to be logged in to edit messages.')
-        return redirect(url_for('auth.login'))
-
-    message = Messages.query.get(message_id)
-    if not message or message.sender != sender:
-        flash('You can only edit your own messages.')
-        return redirect(url_for('messaging.view_messages', recipient=message.recipient))
-
-    new_content = request.form.get('message')
-    if len(new_content) > 1000:
-        flash('Message cannot exceed 1000 characters.')
-        return redirect(url_for('messaging.view_messages', recipient=message.recipient))
-
-    message.content = new_content
-    message.edited = True
     db.session.commit()
 
-    flash('Message edited successfully!')
-    return redirect(url_for('messaging.view_messages', recipient=message.recipient))
-'''
+    return redirect(url_for('messaging.view_messages', recipient=recipient_username))
